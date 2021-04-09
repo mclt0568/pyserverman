f0ec9ee51a1767fd2a611b97ad57732667c81537
Frankium <tis7bfrankie@gmail.com>
2021-04-09 11:25:41 +0800

    add custom exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,9 @@
 class LoggedExceptions(Exception):
+	def __init__(self,message, logger=None):
+		self.message = message
+		if logger:
+			logger.log(message, logtype="error")
+		super().__init__(self.message)
+
+class ConfigError(LoggedExceptions):
 	pass
