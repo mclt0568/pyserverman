c74306df24d1e81439466bb9545effcb6e2d90c9
Frankium <tis7bfrankie@gmail.com>
2021-04-10 10:46:41 +0800

    rearringed sequence for importing in global_modules

--- global_modules.py
+++ global_modules.py
@@ -1,8 +1,9 @@
 import config_controls as c
 import logger as l
-import discord_wrapper as d
 
 logger = l.logger()
 configs = c.ConfigControls()
 configs.read_configs()
+
+import discord_wrapper as d
 discord_client = d.DiscordWrapper()
