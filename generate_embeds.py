20a2eb39ea11c40dd677dbef968a5245c6205f6b
Frankium <tis7bfrankie@gmail.com>
2021-04-10 13:08:27 +0800

    add funtions to generate discord.Embed

---
+++ generate_embeds.py
@@ -0,0 +1,11 @@
+import discord
+
+def generate_error(title,description,prompt,message):
+	embed = discord.Embed(title=f"錯誤: {title}", description=description, color=discord.Color.from_rgb(229, 115, 115))
+	embed.add_field(name="提示",value=prompt,inline=False)
+	embed.add_field(name="Traceback",value=f"`{message.content.strip()}`",inline=False)
+	return embed
+
+def generate_success(description):
+	embed = discord.Embed(title=f"指令已完成", description=description, color=discord.Color.from_rgb(129, 199, 132))
+	return embed
