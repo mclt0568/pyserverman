fffd7dfd1ed048d3d4787be8f7d49c9427bee28c
Frankium <tis7bfrankie@gmail.com>
2021-04-10 10:28:21 +0800

    adds discord wrapper instance to global_modules

--- global_modules.py
+++ global_modules.py
@@ -1,6 +1,8 @@
 import config_controls as c
 import logger as l
+import discord_wrapper as d
 
 logger = l.logger()
 configs = c.ConfigControls()
 configs.read_configs()
+discord_client = d.DiscordWrapper()
