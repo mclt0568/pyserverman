5b586b191325064d566918c55dde3147791d9637
Frankium <tis7bfrankie@gmail.com>
2021-04-10 13:15:36 +0800

    accpets target list for successful embeds

--- generate_embeds.py
+++ generate_embeds.py
@@ -6,6 +6,8 @@ def generate_error(title,description,prompt,message):
 	embed.add_field(name="Traceback",value=f"`{message.content.strip()}`",inline=False)
 	return embed
 
-def generate_success(description):
+def generate_success(description,targets=None):
 	embed = discord.Embed(title=f"指令已完成", description=description, color=discord.Color.from_rgb(129, 199, 132))
+	if targets:
+		embed.add_field(name="對象",value="\n".join(targets),inline=False)
 	return embed
