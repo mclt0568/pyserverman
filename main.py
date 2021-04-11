c6ed626ecebc1b7cd72323344e78fe66c0497b42
Leung Yau Ming <leungyauming2005@gmail.com>
2021-04-11 18:02:52 +0800

    python run servers on start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 import os
+import mc
 import intentions
 
 
@@ -8,14 +9,17 @@ def main():
     if not os.path.isdir(servers_dir_name):
         os.makedirs(servers_dir_name)
     servers = config["servers"]
+    server_objs = []
     for server in servers:
         server_name = server["name"]
         server_dir_name = os.path.join(servers_dir_name, server_name)
         if not os.path.isdir(server_dir_name):
             os.makedirs(server_dir_name)
 
-        # server_script = server["script"]
-        # server_args = server["args"]
+        server_objs.append(mc.Server(server["script"]))
+    
+    for server_obj in server_objs:
+        server_obj.run()
 
     bot.run(config["bot"]["token"])
 
