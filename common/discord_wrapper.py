fa2b21b7e6ec47a082cf3f0172c535e57b960a78
Leung Yau Ming <leungyauming2005@gmail.com>
2021-04-11 10:23:33 +0800

    remove message .lower()

--- common/discord_wrapper.py
+++ common/discord_wrapper.py
@@ -33,21 +33,18 @@ class DiscordWrapper(discord.Client):
         return raw_intention[0] == "[" and raw_intention[-1] == "]" and raw_intention in self.intentions
 
     async def on_message(self, message):
-        raw_msg = message.content
-        msg = raw_msg.strip().lower()
+        msg = message.content.strip()
         if not msg:
             return
 
         msg_pieces = msg.split(" ")
 
-        if self.is_intention(msg_pieces[0]):
+        if self.is_intention(msg_pieces[0].lower()):
             self.logger.log(
-                f"{message.author.name}#{message.author.discriminator} is trying to execute the following intention: {msg_pieces[0]}")
-            self.logger.log(f"Original message goes like:")
-            self.logger.log(f"\t{message.content}")
+                f"{message.author.name}#{message.author.discriminator} executed: {msg}")
 
             if message.author.id not in self.config["admins"]:
                 await message.channel.send("權限不足")
                 return
 
-            await self.intentions[msg_pieces[0]](self, message, raw_msg.split(" ")[1:])
+            await self.intentions[msg_pieces[0]](self, message, msg_pieces[1:])
