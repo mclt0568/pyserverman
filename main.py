036b4247d1e4521074ab79f5f08caf619dc7358d
Leung Yau Ming <leungyauming2005@gmail.com>
2021-04-11 17:39:55 +0800

    fix missing not

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import intentions
 
 def main():
     servers_dir_name = config["servers_dir"]
-    if os.path.isdir(servers_dir_name):
+    if not os.path.isdir(servers_dir_name):
         os.makedirs(servers_dir_name)
     servers = config["servers"]
     for server in servers:
         server_name = server["name"]
         server_dir_name = os.path.join(servers_dir_name, server_name)
-        if os.path.isdir(server_dir_name):
+        if not os.path.isdir(server_dir_name):
             os.makedirs(server_dir_name)
 
         # server_script = server["script"]
