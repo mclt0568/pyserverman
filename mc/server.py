a51faaa03065bfc92cf64d634b84afc45ecd0220
Frankium <tis7bfrankie@gmail.com>
2021-04-12 19:27:07 +0800

    restructured and generalized server class modules

--- mc/server.py
+++ mc/server.py
@@ -11,7 +11,7 @@ class Server(threading.Thread):
     script: str
     config: common.Config
     logger: logging.Logger
-
+    
     server_process: subprocess.Popen
 
     def __init__(self, name: str, script: str, config: common.Config, logger: logging.Logger) -> None:
@@ -23,25 +23,27 @@ class Server(threading.Thread):
         self.logger = logger
 
     def run(self):
+        self.logger.log(f"Started server: {self.name}")
         self.server_process = subprocess.Popen(
             shlex.split(self.script),
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             cwd=os.path.join(self.config["servers_dir"], self.name)
         )
-
-        try:
-            while True:
-                # blocking when no content is in stdout
-                stdout_line_bytes = self.server_process.stdout.readline()
-                if stdout_line_bytes == b"" and self.server_process.poll() != None:
-                    break
-        except KeyboardInterrupt:
-            self.logger.log("Received terminate signal!")
-            self.logger.log("Terminating subprocesses . . . ")
-            self.server_process.terminate()
-            self.logger.log("Terminated all subprocesses!")
+        while True:
+            if not self.is_running():
+                self.logger.log(f"Server has stopped: {self.name}")
+                break
 
     def run_command(self, command: str):
+        self.logger.log(f"Executed command on {self.name}: {command}")
         self.server_process.stdin.write(f"{command}\r\n".encode())
         self.server_process.stdin.flush()
+
+    def is_running(self):
+        stdout_line_bytes = self.server_process.stdout.readline()
+        return stdout_line_bytes != b"" or self.server_process.poll() == None
+
+    def terminate(self):
+        self.logger.log(f"Server terminated manually: {self.name}")
+        self.server_process.terminate()
