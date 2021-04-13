7f98de1d261ee982df60c24be8d650965477a3e9
Leung Yau Ming <leungyauming2005@gmail.com>
2021-04-13 18:51:38 +0800

    use one underscore for private attributes

--- db/database.py
+++ db/database.py
@@ -8,24 +8,24 @@ from typing import Any, Iterable
 # one and only one object should be used for one database file
 class Database:
     def __init__(self, filename: str) -> None:
-        self.__db_conn = sqlite3.connect(filename, check_same_thread=False)
-        self.__db_conn_lock = threading.Lock()
+        self._db_conn = sqlite3.connect(filename, check_same_thread=False)
+        self._db_conn_lock = threading.Lock()
 
     def __del__(self) -> None:
-        self.__db_conn.close()
+        self._db_conn.close()
 
     # create new cursor and return the new cursor
-    def __new_cursor(self) -> sqlite3.Cursor:
-        return self.__db_conn.cursor()
+    def _new_cursor(self) -> sqlite3.Cursor:
+        return self._db_conn.cursor()
 
     # save changes to file
     def save(self) -> None:
-        with self.__db_conn_lock:
-            self.__db_conn.commit()
+        with self._db_conn_lock:
+            self._db_conn.commit()
 
     # execute sql and save
     def execute(self, sql: str, args: Iterable = ()) -> None:
-        cursor = self.__new_cursor()
+        cursor = self._new_cursor()
         cursor.execute(sql, args)
 
         self.save()
@@ -35,7 +35,7 @@ class Database:
     # execute sql, fetch one row and return result
     # return None if no result
     def query_one(self, sql: str, args: Iterable = ()) -> Any:
-        cursor = self.__new_cursor()
+        cursor = self._new_cursor()
         cursor.execute(sql, args)
 
         results = cursor.fetchone()
@@ -46,7 +46,7 @@ class Database:
     # execute sql, fetch {wanted_results_count} lines of rows and return results
     # return None if no results
     def query_many(self, wanted_results_count: int, sql: str, args: Iterable = ()) -> Any:
-        cursor = self.__new_cursor()
+        cursor = self._new_cursor()
         cursor.execute(sql, args)
 
         results = cursor.fetchmany(wanted_results_count)
@@ -59,7 +59,7 @@ class Database:
     # execute sql, fetch all returned rows and return the rows
     # return None if no returned rows
     def query_all(self, sql: str, args: Iterable = ()) -> Any:
-        cursor = self.__new_cursor()
+        cursor = self._new_cursor()
         cursor.execute(sql, args)
 
         results = cursor.fetchall()
