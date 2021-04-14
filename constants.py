7e725edc15e37d24373211bfbd528e76a7b2c58b
Frankium <tis7bfrankie@gmail.com>
2021-04-14 14:38:25 +0800

    restructure constants to curent database class structure

--- constants.py
+++ constants.py
@@ -5,8 +5,17 @@ import mc
 import db
 import admins
 
+#initialization of Database
 database = db.Database("data.db")
+#initialization of Tables
+database.init_table(
+	"admins",
+	{
+		"user_id":str,
+	}
+)
 
+#initialize common objects
 config = common.Config()
 logger = common.Logger()
 bot = dc.Bot(config, logger)
