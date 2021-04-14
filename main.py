9b83b840c5f6058ecd4b6f5916a534b8249654a2
Frankium <tis7bfrankie@gmail.com>
2021-04-14 16:52:03 +0800

    clean up main

--- main.py
+++ main.py
@@ -3,23 +3,12 @@ import constants
 import mc
 import os
 
-
 def main():
     servers_dir_name = config["servers_dir"]
     if not os.path.isdir(servers_dir_name):
         os.makedirs(servers_dir_name)
 
-    # server_name = server_json_obj["name"]
-    # server_dir_name = os.path.join(servers_dir_name, server_name)
-    # if not os.path.isdir(server_dir_name):
-    #     os.makedirs(server_dir_name)
-
-    # server_obj = mc.Server(
-    #     server_json_obj["name"], server_json_obj["script"], config, logger)
-
-    # constants.servers.append(server_obj)
-
-    import intentions  # to register all intentions
+    import intentions
     bot.run(config["bot"]["token"])
 
 
